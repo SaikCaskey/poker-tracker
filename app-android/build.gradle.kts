35b69b6c21c40509233dbcc48fb6d5bdb40b1539
Alex Caskey <floreat.bangoria@gmail.com>
2025-06-12 18:31:31 +0200

    Update build.gradle.kts
    bump version to update tag

--- app-android/build.gradle.kts
+++ app-android/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.saikcaskey.github.pokertracker.fdroid"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 2
+        versionName = "0.1.1"
     }
 
     packaging {
